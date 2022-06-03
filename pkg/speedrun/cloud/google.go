6e31259f425b85a942727a13f6c0bc086d5523e3
Leo Palmer <leo.sunmo@gmail.com>
2022-06-03 15:30:18 +0200

    Update GCP provider
    Make it fit with provider interface.
    Add project requirement to the provider code.
    Add proper context propagation.

--- pkg/speedrun/cloud/google.go
+++ pkg/speedrun/cloud/google.go
@@ -2,34 +2,45 @@ package cloud
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
+	"github.com/spf13/viper"
 	"google.golang.org/api/compute/v1"
+	"google.golang.org/api/option"
 )
 
 type GoogleClient struct {
+	project string
 	*compute.Service
 }
 
-func NewGCPClient() (*GoogleClient, error) {
+var (
+	// ErrProjectMissing is returned when the project ID is missing
+	ErrProjectMissing = errors.New("missing Google Cloud project ID")
+)
+
+func newGCPClient() (*GoogleClient, error) {
 	var err error
 	ctx := context.Background()
 
-	gce, err := compute.NewService(ctx)
+	gce, err := compute.NewService(ctx,
+		option.WithCredentialsFile(viper.GetString("gcp.credentials")))
 	if err != nil {
 		err = fmt.Errorf("couldn't initialize GCP client: %v", err)
 		return nil, err
 	}
 
-	return &GoogleClient{gce}, nil
+	return &GoogleClient{
+		project: viper.GetString("gcp.project"),
+		Service: gce,
+	}, nil
 }
 
-// GetInstances returns a list of external IP addresses used for the SHH connection
-func (c *GoogleClient) GetInstances(project string) ([]Instance, error) {
+// GetInstances returns a list of external IP addresses used for the SSH connection
+func (c *GoogleClient) getInstances(ctx context.Context) ([]Instance, error) {
 	instances := []Instance{}
-	listCall := c.Instances.AggregatedList(project).Fields("nextPageToken", "items(Name,NetworkInterfaces,Labels)")
-	var ctx context.Context
-
+	listCall := c.Instances.AggregatedList(c.project).Fields("nextPageToken", "items(Name,NetworkInterfaces,Labels)")
 	listCall.Pages(ctx, func(list *compute.InstanceAggregatedList) error {
 		for _, item := range list.Items {
 			for _, instance := range item.Instances {
